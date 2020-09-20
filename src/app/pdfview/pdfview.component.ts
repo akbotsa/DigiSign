ee542b3cbebe0aff166d6a0a871c41391db41330
Ganesh Pathala <macsoftechnologies@gmail.com>
2020-09-20 19:59:42 +0530

    modifications in inbox and sent and pdfview compoennts

--- src/app/pdfview/pdfview.component.ts
+++ src/app/pdfview/pdfview.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ServicesService } from '../services/services.service';
-import {NgbModal,  ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbModal,
+  ModalDismissReasons,
+  NgbModalRef,
+} from '@ng-bootstrap/ng-bootstrap';
 declare var jquery: any;
 declare var $: any;
 
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
-
-
 @Component({
   selector: 'app-pdfview',
   templateUrl: './pdfview.component.html',
-  styleUrls: ['./pdfview.component.css']
+  styleUrls: ['./pdfview.component.css'],
 })
 export class PdfviewComponent implements OnInit {
   viewSrc: string;
@@ -34,25 +36,28 @@ export class PdfviewComponent implements OnInit {
   initialFile: any;
   dummy = [];
   isShowflag: boolean = false;
-  @ViewChild("mymodal",{static: false})mymodal:TemplateRef<any>
+  @ViewChild('mymodal', { static: false }) mymodal: TemplateRef<any>;
 
-  constructor(private modalService: NgbModal, private services: ServicesService, private router:Router, private toastr: ToastrService) { }
+  constructor(
+    private modalService: NgbModal,
+    private services: ServicesService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    
     this.userId = JSON.parse(localStorage.getItem('userDetails'))._id;
-    this.docID = localStorage.getItem("docId");
-    this.docfile = localStorage.getItem("docfile");
+    this.docID = localStorage.getItem('docId');
+    this.docfile = localStorage.getItem('docfile');
     this.viewSrc = `http://15.207.202.132:7000/api/v1/documents/document/${this.docfile}`;
     this.loadRecipientsList();
   }
 
   loadRecipientsList() {
-
     let reqObj = {
       UserId: this.userId,
-      DocId: this.docID
-    }
+      DocId: this.docID,
+    };
 
     /* this.services.recipientsList(reqObj).subscribe((resp) => {
       console.log("recpList resp-----", resp);
@@ -63,82 +68,85 @@ export class PdfviewComponent implements OnInit {
       this.viewSrc = `http://15.207.202.132:7000/api/v1/documents/document/${docum}`;
     }) */
 
-
     this.services.getpdfcoordinates(reqObj).subscribe((resp) => {
-      if(resp.statusCode == 200){
+      if (resp.statusCode == 200) {
         console.log('coordinats-->', resp);
         this.userData = resp.data[0].Recipients;
-  
+
         this.useRecId = resp.data[0].Recipients[0].ReceiptId;
         this.userDocId = resp.data[0].DocId;
         console.log(this.userData);
-      }else if(resp.statusCode == 403){
-        this.toastr.error('Waiting for prior authorized signatures.','Failed:')
-      }else{
-        this.toastr.error('Oops! Somthing went worng.','Failed:')
+      } else if (resp.statusCode == 403) {
+        this.isShowflag = true;
+        this.toastr.error(
+          'Waiting for prior authorized signatures.',
+          'Failed:'
+        );
+      } else {
+        this.toastr.error('Oops! Somthing went worng.', 'Failed:');
       }
-      if(this.userData.length > 0){
+      if (this.userData.length > 0) {
         for (let i = 0; i < this.userData.length; i++) {
           console.log('verify-->', this.userData[i].VerifyFlag);
-            if(this.userData[i].VerifyFlag === true){
-                this.dummy.push(this.userData[i].VerifyFlag);
-            }
+          if (this.userData[i].VerifyFlag === true) {
+            this.dummy.push(this.userData[i].VerifyFlag);
+          }
         }
       }
 
-      if(this.dummy.length >0){
+      if (this.dummy.length > 0) {
         this.isShowflag = true;
       }
-
-      
-    })
+    });
 
     //this.userData = JSON.parse(localStorage.getItem('userData'));
     console.log(this.userData);
   }
 
   draggable_Signature(id) {
-    console.log(id)
+    console.log(id);
     this.modalService.open(this.mymodal);
     //this.modelref.close();
-    console.log("method working");
-    localStorage.setItem('itemId',id);
+    console.log('method working');
+    localStorage.setItem('itemId', id);
   }
 
   saveImage(data) {
-    let self= this;
+    let self = this;
     var reader = new FileReader();
-    reader.readAsDataURL(data); 
-    reader.onloadend = function() {
-        var base64data = reader.result;                
-        self.itemId = localStorage.getItem('itemId');
-        if(self.itemId == 1){
-          var xx = '.drag';
-          var yy = '.remove';
-          var aa = 'remove';
-          self.signatureImage = data;
-        }else{
-          var xx = '.idrag';
-          var yy = '.iremove';
-          var aa = 'iremove';
-          self.initialFile = data;
-        }
-        $(yy).remove();
-        $(xx).css({"background-color": "transparent", "padding": 0});
-        $(xx).append(`<img class="${aa}" style="height: 70px;width: 80px;" src="${base64data}">`);
-        self.modalService.dismissAll();
-    }
+    reader.readAsDataURL(data);
+    reader.onloadend = function () {
+      var base64data = reader.result;
+      self.itemId = localStorage.getItem('itemId');
+      if (self.itemId == 1) {
+        var xx = '.drag';
+        var yy = '.remove';
+        var aa = 'remove';
+        self.signatureImage = data;
+      } else {
+        var xx = '.idrag';
+        var yy = '.iremove';
+        var aa = 'iremove';
+        self.initialFile = data;
+      }
+      $(yy).remove();
+      $(xx).css({ 'background-color': 'transparent', padding: 0 });
+      $(xx).append(
+        `<img class="${aa}" style="height: 70px;width: 80px;" src="${base64data}">`
+      );
+      self.modalService.dismissAll();
+    };
   }
 
   onFileSelected(event) {
-    console.log("onFileSelected--->", event);
+    console.log('onFileSelected--->', event);
     const file = (event.target as HTMLInputElement).files[0];
     this.fileContent = file;
     const reader = new FileReader();
     reader.onload = () => {
       this.imageURL = reader.result as string;
-    }
-    reader.readAsDataURL(file)
+    };
+    reader.readAsDataURL(file);
   }
 
   uploadSignature() {
@@ -180,40 +188,41 @@ export class PdfviewComponent implements OnInit {
       })
     } */
 
-
-    console.log("uploadSignature--->", this.fileContent);
+    console.log('uploadSignature--->', this.fileContent);
     const reader = new FileReader();
     reader.onload = () => {
-      console.log("res--->",reader.result );
+      console.log('res--->', reader.result);
       this.mainImage = reader.result as string;
 
       /* append image here */
-      
-      if(this.itemId == 1){
+
+      if (this.itemId == 1) {
         var xx = '.drag';
         var yy = '.remove';
         var aa = 'remove';
         this.signatureImage = this.fileContent;
-      }else{
+      } else {
         var xx = '.idrag';
         var yy = '.iremove';
         var aa = 'iremove';
-        this.initialFile =  this.fileContent;
+        this.initialFile = this.fileContent;
       }
-      console.log("yy--->",yy);
-      console.log("xx--->",xx);
+      console.log('yy--->', yy);
+      console.log('xx--->', xx);
       $(yy).remove();
-      $(xx).css({"background-color": "transparent", "padding": 0});
-      $(xx).append(`<img class="${aa}" style="height: 70px;width: 300px;" src="${this.mainImage}">`);
+      $(xx).css({ 'background-color': 'transparent', padding: 0 });
+      $(xx).append(
+        `<img class="${aa}" style="height: 70px;width: 300px;" src="${this.mainImage}">`
+      );
 
       console.log(this.uploadedFiles);
 
       this.modalService.dismissAll();
-    }
+    };
     reader.readAsDataURL(this.fileContent);
   }
 
-  updateSignature(){
+  updateSignature() {
     const formData = new FormData();
     formData.append('DocId', this.userDocId);
     formData.append('RecipientID', this.useRecId);
@@ -223,14 +232,13 @@ export class PdfviewComponent implements OnInit {
     //console.log('formData--->', formData);
 
     this.services.sendRecipientFiles(formData).subscribe((resp) => {
-      if(resp.statusCode == 200){
-        this.toastr.success(`${resp.Message}`,'Success:')
-        this.router.navigateByUrl('/document')
-      }else{
-        this.toastr.error(`${resp.Message}`,'Failed:')
+      if (resp.statusCode == 200) {
+        this.toastr.success(`${resp.Message}`, 'Success:');
+        this.router.navigateByUrl('/document');
+      } else {
+        this.toastr.error(`${resp.Message}`, 'Failed:');
       }
       console.log('coordinats-->', resp);
-    })
+    });
   }
-
 }
