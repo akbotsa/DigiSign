d895fa72f6fbe22c0d07de191fcb18d0411c153f
sanjeev644 <sanjeev@macsof.com>
2020-10-28 13:35:20 +0530

    changes in inbox  sent

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,19 +6,19 @@ declare var baseUrl: any;
 
 export const environment = {
   production: false,
-  baseUrl : "http://15.207.202.132:7000/api/v1/",
-  imageBaseUrl : "http://15.207.202.132:7000/api/v1/documents/document/",
+  // baseUrl : "http://15.207.202.132:7000/api/v1/",
+  // imageBaseUrl : "http://15.207.202.132:7000/api/v1/documents/document/",
   // baseUrl : "http://localhost:7000/api/v1/",
   // imageBaseUrl : "http://localhost:7000/api/v1/documents/document/",
-  // baseUrl : "https://app.getendorse.co/api/v1/",
-  // imageBaseUrl : "https://app.getendorse.co/api/v1/documents/document/", 
+  baseUrl : "https://app.getendorse.co/api/v1/",
+  imageBaseUrl : "https://app.getendorse.co/api/v1/documents/document/", 
 
 };
 
 /*  
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
+ *  
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
